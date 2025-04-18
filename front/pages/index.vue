24fe4f490103faae9fcd79eb9391c003d0d89dcf
spl3g <spleefer6@yandex.ru>
2025-04-18 19:34:32 +0300

    Add pages

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-	<div></div>
+	<div class="flex flex-row gap-4">
+		<div class="bg-red-200 basis-1/4">
+			<DocumentList />
+		</div>
+		<div class="bg-red-100 basis-2/4">< /></div>
+		<div class="bg-red-200 basis-1/4">< /></div>
+	</div>
 </template>
