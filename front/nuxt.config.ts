4559e9d27b1a93b3ba3237fce4410bcd86ae8dfe
MomusWinner <areg06.06@mail.ru>
2025-04-19 07:49:45 +0300

    Add mark down

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,17 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-	compatibilityDate: "2025-04-19",
-	modules: ["shadcn-nuxt", "@pinia/nuxt", "@vee-validate/nuxt"],
-	devtools: { enabled: true },
-	css: ["~/assets/css/tailwind.css"],
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	ssr: false,
+				compatibilityDate: "2025-04-19",
+				modules: ["shadcn-nuxt", "@pinia/nuxt", "@vee-validate/nuxt"],
+				devtools: { enabled: true },
+				css: ["~/assets/css/tailwind.css"],
+				vite: {
+								plugins: [tailwindcss()],
+				},
+				ssr: false,
 
-	shadcn: {
-		prefix: "",
-		componentDir: "./components/ui",
-	},
+				shadcn: {
+								prefix: "",
+								componentDir: "./components/ui",
+				},
 });
