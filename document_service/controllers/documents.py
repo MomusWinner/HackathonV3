d77265b7b0e4e0af2fd77573a138961c45cb40b1
akiko23 <usksoaoko@gmail.com>
2025-04-19 05:03:56 +0300

    fix websockets #1

--- document_service/controllers/documents.py
+++ document_service/controllers/documents.py
@@ -2,15 +2,18 @@ import asyncio
 from contextlib import suppress
 from typing import Annotated
 from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
 
 from dishka import FromDishka
 from dishka.integrations.fastapi import DishkaRoute, inject
 from fastapi import APIRouter, HTTPException, status, UploadFile, Form, WebSocket
 from redis.asyncio import Redis
 
+from document_service.repositories.document_repository import DocumentRepository
 from document_service.schemas.document import DocumentCreate, DocumentResponse, \
     ManyDocumentsResponse
 from document_service.services.document_service import DocumentService
+from document_service.tasks.ai_tasks import AIRemoteDocumentAnalyzer
 from document_service.utils.cache import cache
 from document_service.utils.metrics import (
     CREATE_DOCUMENT_METHOD_DURATION,
@@ -53,30 +56,37 @@ async def create_documents(
 async def receive_analyze(
         websocket: WebSocket,
         document_id: UUID,
-        service: FromDishka[DocumentService]
+        session_maker: FromDishka[async_sessionmaker]
 ):
+
     await websocket.accept()
     if document_id not in websocket_connections:
         websocket_connections[document_id] = []
     websocket_connections[document_id].append(websocket)
 
-    try:
-        while True:
-            # Держим соединение открытым
-            # await websocket.receive_text()
-            print(f'Checking database for {document_id}..')
-            document = await service.get_document(document_id)
-            if document is not None and document.processing_status == 'completed':
-                await websocket.send_json(data=document.model_dump(mode='json'))
-            await asyncio.sleep(2)
-    except Exception as e:
-        print(e)
-        websocket_connections[document_id].remove(websocket)
-        if not websocket_connections[document_id]:
-            del websocket_connections[document_id]
-    finally:
-        with suppress(RuntimeError):
-            await websocket.close()
+    async with session_maker() as session:
+        service = DocumentService(
+            DocumentRepository(session),
+            AIRemoteDocumentAnalyzer()
+        )
+
+        try:
+            while True:
+                # Держим соединение открытым
+                # await websocket.receive_text()
+                print(f'Checking database for {document_id}..')
+                document = await service.get_document(document_id)
+                if document is not None and document.processing_status == 'completed':
+                    await websocket.send_json(data=document.model_dump(mode='json'))
+                await asyncio.sleep(2)
+        except Exception as e:
+            print(e)
+            websocket_connections[document_id].remove(websocket)
+            if not websocket_connections[document_id]:
+                del websocket_connections[document_id]
+        finally:
+            with suppress(RuntimeError):
+                await websocket.close()
 
 
 websocket_connections = {}
